1d12fe14586dac36c897329e73de67adbe00abc2
rfgIII <rfg17@pitt.edu>
2026-03-05 17:15:04 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  top_um_mult_top user_project (
+  tt_um_mult_top user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
